6488fa8692c2b23d6c929a786a1b6958b0b032f3
godson.kileo <godson.kileo@ports.go.tz>
2025-02-19 17:19:43 +0300

    Recipe apis added

--- app/app/urls.py
+++ app/app/urls.py
@@ -35,8 +35,8 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
     ),
-    # path('api/user/', include('user.urls')),
-    # path('api/recipe/', include('recipe.urls')),
+    path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ]
 
 if settings.DEBUG:
---
+++ app/recipe/urls.py
@@ -0,0 +1,18 @@
+""" URL for mapping recipe APP."""
+
+from django.urls import (
+    path,
+    include,
+)
+from rest_framework.routers  import DefaultRouter
+from recipe import views
+
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+app_name= 'recipe'
+urlpatterns = [ 
+    path('', include(router.urls)),
+    
+]
+
